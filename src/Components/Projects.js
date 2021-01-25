3b055da7e930d0a52bf019645ec543a8c74bd7fe
Jesus Reyes <jesusareyes01@gmail.com>
2021-01-25 16:08:35 -0600

    changed carousel in phone and ipad view. added 2 more skills.

--- src/Components/Projects.js
+++ src/Components/Projects.js
@@ -3,7 +3,7 @@ import {imgDir} from '../assets/projects/imgDir';
 import Carousel from 'react-bootstrap/Carousel';
 
 const Projects = () => {
-
+    //Mapping Scan&Go project images and title
     const scanGoImgMapped = imgDir.projects.scango.map((img, i) => {
         return(
             <Carousel.Item key={i}>
@@ -16,7 +16,7 @@ const Projects = () => {
             )
         }
     )
-
+    //Mapping Journalize project images and title
     const journalizeImgMapped = imgDir.projects.journalize.map((img, i) => {
         return(
             <Carousel.Item key={i}>
@@ -29,7 +29,7 @@ const Projects = () => {
             )
         }
     )
-
+    //Mapping Pixel Art project images and title
     const pixelArtImgMapped = imgDir.projects.pixelArt.map((img, i) => {
         return(
             <Carousel.Item key={i}>
@@ -47,10 +47,11 @@ const Projects = () => {
     return(
         <section id='projects' className='projects-section'>
             <h2 className='projects-title'>PROJECTS</h2>
-
+            {/* Scan & Go PROJECT SECTION */}
             <article className='scango'>
                 <h2>Scan & Go</h2>
-                <p><strong>Technologies:</strong> React, Node</p>
+                
+                <p><strong>Technologies:</strong> React, Node, PostgreSQL, Heroku </p>
                 <p><strong>Context:</strong> Devmountain Group Project with 2 week time limit. 
                     The goal was to make a full stack site that is intuitive, user friendly, and engaging while working on a team.</p>
                 <p><strong>Description:</strong> Scan&Go is a site that allows shoppers to use their phones to scan items then pay on their device. An 'exit pass' is 
@@ -58,17 +59,20 @@ const Projects = () => {
                 <p><strong>Login credentials:</strong> Email: test@gmail.com Password: test , or feel free to register a new user.</p>
                 <p><strong>Test barcodes/UPC:</strong> 9780979305061, 9781594631870, 9780349411903, 9780439662093 </p>
                 <p><strong>Checkout process:</strong> Test credit card number is 4242 4242 4242 4242, Enter any future date for expiration and CV and zip code can be any number.</p>
+                
                 <div className='project-links'>
                     <a href="https://scanandgo.jesusreyes.dev/">DEMO</a>
                     <a href="https://github.com/Mobile-check-out-team/mobile-checkout">SOURCE</a>
                 </div>
+
                 <Carousel className='scanGoCarousel'>
                     {scanGoImgMapped}
                 </Carousel>
             </article>
-
+            {/* Journalize PROJECT SECTION */}
             <article className='journalize'>
                 <h2>Journalize</h2>
+
                 <p><strong>Technologies:</strong> React, Node</p>
                 <p><strong>Context:</strong> Devmountain Personal Project with 2 week time limit. The goal 
                     was to make a full stack site that is fun, responsive, and easy to navigate.</p>
@@ -85,9 +89,10 @@ const Projects = () => {
                     {journalizeImgMapped}
                 </Carousel>
             </article>
-
+            {/* Pixel Art PROJECT SECTION */}
             <article className='pixelArt'>
                 <h2>Pixal Art</h2>
+
                 <p><strong>Technologies:</strong> React, Node</p>
                 <p><strong>Description:</strong>  Pixel Art is free online pixel drawing tool that allows you to make pixel art.</p>
 
@@ -95,11 +100,12 @@ const Projects = () => {
                     <a href="https://pixelart.jesusreyes.dev/">DEMO</a>
                     <a href="https://github.com/JesusReyes01/Pixel-Art-React">SOURCE</a>
                 </div>
+
                 <Carousel className='pixelArtCarousel'>
                     {pixelArtImgMapped}
                 </Carousel>
             </article>
-
+            {/* Portfolio GITHUB LINK SECTION */}
             <article className='portfolio'>
                 <p>View this portfolio site's source code on <a href='https://github.com/JesusReyes01/portfolio'>GitHub</a></p>
 
